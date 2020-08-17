f5860fc5783954e265d39dcaff64a8bfd4f474af
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-08-17 11:57:26 +0500

    compareSign handler

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const http = require('http');
 const https = require('https');
 const express = require('express');
 const cors = require('cors');
-// const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const qs = require('querystring');
-const crypto = require('crypto');
 
 dotenv.config();
 
@@ -21,29 +18,7 @@ const {handleError, createError} = require("./handlers/error");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use((req, res, next) => {
-	const params = req.query;
-	const ordered = {};
-	Object.keys(params).sort().forEach((key) => {
-		if (key.slice(0, 3) === 'vk_') {
-			ordered[key] = params[key];
-		}
-	});
-	const stringParams = qs.stringify(ordered);
-	const paramsHash = crypto
-		.createHmac('sha256', process.env.APP_VK)
-		.update(stringParams)
-		.digest()
-		.toString('base64')
-		.replace(/\+/g, '-')
-		.replace(/\//g, '_')
-		.replace(/=$/, '');
-	if (paramsHash === params.sign) {
-		next();
-	} else {
-		next(createError(403, 'Ошибка параметров подписи'));
-	}
-});
+app.use(require('./handlers/compareSign'));
 
 app.use(require('./routes'));
 
